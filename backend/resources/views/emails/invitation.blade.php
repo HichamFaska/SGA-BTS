1b69c302784a70a4c089dfe7a710c2c02a4b4371
Faska <hichamfaska50@gmail.com>
2026-06-02 20:46:31 +0100

    feat(mail): add invitation email blade template

---
+++ backend/resources/views/emails/invitation.blade.php
@@ -0,0 +1,23 @@
+@component('mail::message')
+
+# Bonjour {{ optional($user->teacher)->first_name ? optional($user->teacher)->first_name.' '.optional($user->teacher)->last_name : $user->email }}
+
+Un compte a été créé pour vous sur la plateforme. Cliquez sur le bouton ci-dessous pour définir votre mot de passe et accéder à votre espace.
+
+@component('mail::button', ['url' => $url, 'color' => 'primary'])
+Activer mon compte
+@endcomponent
+
+Ce lien expirera le {{ $invitation->expires_at->translatedFormat('d F Y H:i') }}.
+
+Si vous n'avez pas demandé cette invitation, vous pouvez ignorer cet e-mail.
+
+Merci,
+
+L'équipe d'administration
+
+@slot('footer')
+Si vous avez des questions, répondez à cet e-mail ou contactez l'administrateur.
+@endslot
+
+@endcomponent
