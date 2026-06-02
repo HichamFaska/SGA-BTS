dcd19ecd6dec3f6fa6b028a31d5d995a1e0cf059
Faska <hichamfaska50@gmail.com>
2026-06-02 20:10:22 +0100

    chore(db): drop unused columns from users table

--- backend/database/migrations/0001_01_01_000000_create_users_table.php
+++ backend/database/migrations/0001_01_01_000000_create_users_table.php
@@ -11,15 +11,10 @@ return new class extends Migration {
 
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
             $table->string('email')->unique();
+            $table->string('password');
+            $table->enum('role', array_column(UserRoleEnum::cases(), 'value'));
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password')->nullable();
-            $table->enum('role', array_column(UserRoleEnum::cases(), 'value'))
-                ->default(UserRoleEnum::STUDENT->value);
-            $table->string('avatar')->nullable();
-            $table->string('address')->nullable();
-            $table->string('phone')->nullable();
             $table->timestamps();
         });
 
