f1fb064bf2fce8ba57dad4a5aae16ef37a90c13b
Faska <hichamfaska50@gmail.com>
2026-06-06 13:39:47 +0100

    feat(filiere): add scopeSearch to Filiere model

--- backend/app/Models/Filiere.php
+++ backend/app/Models/Filiere.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
@@ -12,10 +13,20 @@ class Filiere extends Model {
 
     protected $fillable = [
         'name',
-        'code'
+        'code',
     ];
 
     public function classes(): HasMany {
         return $this->hasMany(Classe::class);
+    }
+
+    public function scopeSearch(Builder $query, ?string $search): Builder {
+        if ($search) {
+            $query->where(function(Builder $q) use ($search) {
+                $q->whereLike('name', "%{$search}%")
+                  ->orWhereLike('code', "%{$search}%");
+            });
+        }
+        return $query;
     }
 }
