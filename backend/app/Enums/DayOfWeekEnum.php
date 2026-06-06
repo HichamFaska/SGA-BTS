2259e5b6fb6af169bfdbaae7f553b3659551e38e
Faska <hichamfaska50@gmail.com>
2026-06-06 02:37:09 +0100

    feat(enums): add DayOfWeekEnum for timetable day values

---
+++ backend/app/Enums/DayOfWeekEnum.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Enums;
+
+enum DayOfWeekEnum: string {
+    case LUNDI = 'lundi';
+    case MARDI = 'mardi';
+    case MERCREDI = 'mercredi';
+    case JEUDI = 'jeudi';
+    case VENDREDI = 'vendredi';
+    case SAMEDI = 'samedi';
+    case DIMANCHE = 'dimanche';
+}
