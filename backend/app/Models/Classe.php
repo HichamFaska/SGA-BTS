1e604a4a58684801f57a94a9634cf9643aec74d3
Faska <hichamfaska50@gmail.com>
2026-06-06 14:50:34 +0100

    feat(classes): add search and filiere scopes to Classe model

--- backend/app/Models/Classe.php
+++ backend/app/Models/Classe.php
@@ -3,11 +3,11 @@
 namespace App\Models;
 
 use App\Enums\ClassLevelEnum;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
-
 
 class Classe extends Model {
     
@@ -29,5 +29,19 @@ class Classe extends Model {
 
     public function students(): HasMany {
         return $this->hasMany(Student::class);
+    }
+
+    public function scopeSearch(Builder $query, ?string $search): Builder {
+        if ($search) {
+            $query->whereLike('name', "%{$search}%");
+        }
+        return $query;
+    }
+
+    public function scopeFiliere(Builder $query, ?int $filiereId): Builder {
+        if ($filiereId) {
+            $query->where('filiere_id', $filiereId);
+        }
+        return $query;
     }
 }
